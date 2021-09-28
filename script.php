cf254c3561418042f320b0d363faec6bc66daa2c
domingueslucaszofoli <91493575+domingueslucaszofoli@users.noreply.github.com>
2021-09-28 10:32:43 -0300

    Validacao de campos do formulario

--- script.php
+++ script.php
@@ -10,6 +10,29 @@ $categorias[] = 'idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if(empty($nome)){
+    echo 'O nome nao pode ser vazio';
+    return;
+}
+
+if (strlen($nome)<3)
+{
+    echo 'O nome deve conter mais que 3 caracteres';
+    return;
+}
+
+if (strlen($nome) > 40)
+{
+    echo 'O nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade))
+{
+     echo 'Informe um numero para idade';
+    return;
+}
+
 if($idade >=6 && $idade <=12)
 {
     for($i=0;$i<= count($categorias);$i++)
