a1ca7a77b9b1ab45a2395d501b0b9cb4f4fe66f4
domingueslucaszofoli <91493575+domingueslucaszofoli@users.noreply.github.com>
2021-09-28 17:01:44 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     session_start();
     ?>
@@ -39,29 +38,4 @@
 
 </body>
 
-=======
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <title>Formulário de Inscrição</title>
-    <meta name="author" content="">
-    <meta nome="description" content="">
-    <meta nome="viewport" content ="width-device-widht, intial-scale=1">
-</head>
-
-<body>
-
-<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
-
-<form action ="script.php" method="post">
-    <p>Seu nome: <Input type="text" name="nome"</p>
-    <p>Sua idade: <Input type="text" name="idade"></p>
-    <p><input type="submit"  value="Enviar dados do competidor"</p>
-</form>
-
-</body>
-
->>>>>>> cf254c3561418042f320b0d363faec6bc66daa2c
 </html>
--- script.php
+++ script.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 session_start();
@@ -84,72 +83,3 @@ else
         }
     }
 }
-=======
-<?php
-
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-$categorias[] = 'idoso';
-//print_r($categorias);
-
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
-
-if(empty($nome)){
-    echo 'O nome nao pode ser vazio';
-    return;
-}
-
-if (strlen($nome)<3)
-{
-    echo 'O nome deve conter mais que 3 caracteres';
-    return;
-}
-
-if (strlen($nome) > 40)
-{
-    echo 'O nome é muito extenso';
-    return;
-}
-
-if(!is_numeric($idade))
-{
-     echo 'Informe um numero para idade';
-    return;
-}
-
-if($idade >=6 && $idade <=12)
-{
-    for($i=0;$i<= count($categorias);$i++)
-    {
-        if($categorias[$i]=='infantil')
-            echo " O nadador " .$nome . " compete na categoria infantil";
-    }
-}
-else if ($idade >=13 && $idade <= 18)
-{
-    for($i=0;$i<= count($categorias);$i++)
-    {
-        if($categorias[$i]=='adolescente')
-            echo " O nadador " .$nome . " compete na categoria adolescente";
-    }
-}
-else if ($idade >18 && $idade < 65)
-{
-    for($i=0;$i<= count($categorias);$i++)
-    {
-        if($categorias[$i]=='adulto')
-            echo " O nadador " .$nome . " compete na categoria adulto";
-    }
-}
-else
-{
-    for($i=0;$i<= count($categorias);$i++)
-    {
-        if($categorias[$i]=='idoso')
-            echo "O nadador " .$nome . " compete na categoria idoso";
-    }
-}
->>>>>>> cf254c3561418042f320b0d363faec6bc66daa2c
