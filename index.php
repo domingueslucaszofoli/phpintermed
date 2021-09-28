947982ea59ed446779b5175ca87ed82293d445e7
domingueslucaszofoli <91493575+domingueslucaszofoli@users.noreply.github.com>
2021-09-28 16:37:13 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+<<<<<<< HEAD
+<?php
+    session_start();
+    ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Formulário de Inscrição</title>
+    <meta name="author" content="">
+    <meta nome="description" content="">
+    <meta nome="viewport" content ="width-device-widht, initial-scale=1">
+</head>
+
+<body>
+
+<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action ="script.php" method="post">
+    <?php
+        $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : '';
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
+    <p>Seu nome: <Input type="text" name="nome"</p>
+    <p>Sua idade: <Input type="text" name="idade"></p>
+    <p><input type="submit"  value="Enviar dados do competidor"</p>
+</form>
+
+</body>
+
+=======
 <!DOCTYPE html>
 <html>
 
@@ -21,4 +63,5 @@
 
 </body>
 
+>>>>>>> cf254c3561418042f320b0d363faec6bc66daa2c
 </html>
--- script.php
+++ script.php
@@ -1,3 +1,90 @@
+<<<<<<< HEAD
+<?php
+
+session_start();
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+if(empty($nome)){
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, preencher novamente';
+    header('location: index.php');
+    return;
+}
+
+else if (strlen($nome)<3)
+{
+    $_SESSION['mensagem-de-erro'] = 'O nome precisa ter no mínimo 3 letras, preencher novamente';
+    header('location: index.php');
+    return;
+}
+
+else if (strlen($nome) > 40)
+{
+    $_SESSION['mensagem-de-erro'] = 'O nome precisa ter no maximo 39 letras, preencher novamente';
+    header('location: index.php');
+    return;
+}
+
+else if(!is_numeric($idade))
+{
+    $_SESSION['mensagem-de-erro'] = 'A idade tem de ser um número';
+    header('location: index.php');
+    return;
+}
+
+if($idade >=6 && $idade <=12)
+{
+    for($i=0;$i<= count($categorias);$i++)
+    {
+        if($categorias[$i]=='infantil') {
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' . $nome . " compete na categoria $categorias[$i]";
+            header('location: index.php');
+            return;
+        }
+    }
+}
+else if ($idade >=13 && $idade <= 18)
+{
+    for($i=0;$i<= count($categorias);$i++)
+    {
+        if($categorias[$i]=='adolescente') {
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' . $nome . " compete na categoria $categorias[$i]";
+            header('location: index.php');
+            return;
+        }
+    }
+}
+else if ($idade >18 && $idade < 65)
+{
+    for($i=0;$i<= count($categorias);$i++)
+    {
+        if($categorias[$i]=='adulto') {
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' . $nome . " compete na categoria $categorias[$i]";
+            header('location: index.php');
+            return;
+        }
+    }
+}
+else
+{
+    for($i=0;$i<= count($categorias);$i++)
+    {
+        if($categorias[$i]=='idoso') {
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador ' . $nome . " compete na categoria $categorias[$i]";
+            header('location: index.php');
+            return;
+        }
+    }
+}
+=======
 <?php
 
 $categorias = [];
@@ -65,3 +152,4 @@ else
             echo "O nadador " .$nome . " compete na categoria idoso";
     }
 }
+>>>>>>> cf254c3561418042f320b0d363faec6bc66daa2c
